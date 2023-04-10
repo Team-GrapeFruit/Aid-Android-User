d28d97d5f8e627a96a0befc1c23e669b61a05659
yeongun130 <yeongunj625@gmail.com>
2023-04-10 16:01:57 +0900

    메뉴페이지 퍼블리싱

---
+++ app/src/main/java/com/grapefruit/aid_android_user/view/menupage/MenuPage.kt
@@ -0,0 +1,12 @@
+package com.grapefruit.aid_android_user.view.menupage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grapefruit.aid.R
+
+class MenuPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_page)
+    }
+}
