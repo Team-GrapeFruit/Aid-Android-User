cee18276dcedf48f2822148b3272506af8babc5f
GSM-Minseok <ms0818000@gmail.com>
2023-05-10 10:11:06 +0900

    firebase realtime database ai answer 읽기 기능 추가

--- app/src/main/java/com/grapefruit/aid_android_user/presentation/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/grapefruit/aid_android_user/presentation/viewmodel/ChatViewModel.kt
@@ -1,8 +1,7 @@
 package com.grapefruit.aid_android_user.presentation.viewmodel
 
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,16 +10,15 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.grapefruit.aid_android_user.data.dto.ChatData
-import com.grapefruit.aid_android_user.data.dto.ShopDetailData
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 class ChatViewModel: ViewModel() {
-    private var _chatInfo = MutableLiveData<ShopDetailData>()
+    private var _chatInfo = MutableLiveData<ChatData>()
     private var chatCnt: Int = 0
 
-    val ChatInfo: LiveData<ShopDetailData>
+    val ChatInfo: LiveData<ChatData>
         get() = _chatInfo
 
     fun sendMessage(message: String){
@@ -28,11 +26,35 @@ class ChatViewModel: ViewModel() {
         val myRef = database.getReference().child("message")
         val time = getTime()
         val dataInput = ChatData(
-            msg = message,
-            time = time
+            message = message,
+            time = time,
+            messageNum = (++chatCnt).toString()
         )
-        myRef.child("user").child("message " + (++chatCnt) ).setValue(dataInput)
+        myRef
+            .child("user")
+            .child("message" + chatCnt )
+            .setValue(dataInput)
         Log.d("msg",chatCnt.toString())
+        //getMessage()
+    }
+    fun getMessage(){
+        val database = FirebaseDatabase.getInstance().getReference()
+            .child("message")
+            .child("ai")
+            .child("answer"+chatCnt)
+
+        database.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.value != null) {
+                    _chatInfo.value = snapshot.getValue(ChatData::class.java)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
     fun getTime(): String{
         val current = LocalDateTime.now()
