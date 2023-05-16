72259c757fd8d47fea83ec0a78a404f9ebc4d5af
yeongun130 <yeongunj625@gmail.com>
2023-05-16 16:05:09 +0900

    프로젝트 구조 정리

--- app/src/main/java/com/grapefruit/aid_android_user/di/NetworkModule.kt
+++ app/src/main/java/com/grapefruit/aid_android_user/di/NetworkModule.kt
@@ -1,13 +1,13 @@
 package com.grapefruit.aid_android_user.di
 
+import com.grapefruit.aid_android_user.data.api.MenuService
+import com.grapefruit.aid_android_user.data.api.PurchaseService
 import com.grapefruit.aid_android_user.data.api.SeatApi
 import com.grapefruit.aid_android_user.data.api.StoreApi
-import com.grapefruit.aid_android_user.data.dto.SeatDTO
-import com.grapefruit.aid_android_user.data.dto.ShopDetailData
+import com.grapefruit.aid_android_user.data.dto.*
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object NetworkModule {
     private const val BASE_URL = "https://port-0-aid-backend-user-nx562oley4rk2t.sel3.cloudtype.app/"
@@ -20,6 +20,38 @@ object NetworkModule {
     val storeApi: StoreApi = retrofit.create(StoreApi::class.java)
 
     val seatApi: SeatApi = retrofit.create(SeatApi::class.java)
+
+    val menuPage: MenuService = retrofit.create(MenuService::class.java)
+
+    val purchasePage: PurchaseService = retrofit.create(PurchaseService::class.java)
+
+    suspend fun menuList(storeId: Long): Response<CheckMenuDTO> {
+        return menuPage.menuList(storeId)
+    }
+
+    suspend fun categoryMenuList(categoryId: Long): Response<CategoryDTO> {
+        return menuPage.menuCategory(categoryId)
+    }
+
+    suspend fun menuDetail(menuId: Long): Response<MenuDetailDTO> {
+        return menuPage.menuDetail(menuId)
+    }
+
+    suspend fun orderMenuToPurchase(seatId: Long, body: List<PurchaseSeatDTO>): Response<Unit> {
+        return purchasePage.orderMenu(seatId, body)
+    }
+
+    suspend fun quantityControl(purchaseId: Long, body: QuantityDTO): Response<Unit> {
+        return purchasePage.quantityControl(purchaseId, body)
+    }
+
+    suspend fun deleteMenu(purchaseId: Long): Response<Unit> {
+        return purchasePage.deleteMenu(purchaseId)
+    }
+
+    suspend fun purchaseList(seatId: Long): Response<List<PurchaseDTO>> {
+        return purchasePage.addMenuList(seatId)
+    }
 
     suspend fun searchStore(storeId: Long): Response<ShopDetailData> {
         return storeApi.searchStore(storeId)
